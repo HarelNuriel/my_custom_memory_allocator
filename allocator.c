475ad36d5bab2ecb57bd602edce46385ae8cf4dc
HarelNuriel <harelnu@gmail.com>
2026-03-20 16:59:09 +0200

    Defragmentation Works.

--- allocator.c
+++ allocator.c
@@ -9,6 +9,7 @@ static uint64_t page_size;
 static struct metadata* first_node;
 
 void release_map() __attribute__((destructor));
+void defragmentation() __attribute__((no_sanitize_address));
 
 void release_map() {
     __asan_unpoison_memory_region(map, page_size);
@@ -31,12 +32,22 @@ int init_mem() {
     first_node->is_free = 1;
     first_node->next_node = NULL;
 
-    __asan_poison_memory_region(first_node + 1, page_size);
+    __asan_poison_memory_region(first_node + 1, page_size - sizeof(struct metadata));
     return 1;
 }
 
 void defragmentation() {
+    struct metadata *node = first_node;
 
+    while (node->next_node != NULL) {
+        const struct metadata* temp = node->next_node;
+        if (node->is_free && temp->is_free) {
+            node->size += temp->size + sizeof(struct metadata);
+            node->next_node = temp->next_node;
+        } else {
+            node = node->next_node;
+        }
+    }
 }
 
 void resize_mem() {
@@ -48,18 +59,13 @@ struct metadata* find_first_free_node(const uint64_t size) {
     int is_fragmented = 1;
     uint64_t page = page_size;
 
-    while (!node->is_free || node->size < size) {
+    while (!node->is_free || node->size < size + sizeof(struct metadata)) {
         if (page == 0) {
             resize_mem();
             // Figure out how to deal with this case after implementing resize_mem
             continue;
         }
-        if (node->next_node == NULL) {
-            struct metadata *next = (void*)node + node->size;
-            next->is_free = 1;
-            next->size = page_size - (page + sizeof(struct metadata));
-        }
-        if (page == node->size) {
+        if (page == node->size + sizeof(struct metadata)) {
             if (is_fragmented) {
                 defragmentation();
                 is_fragmented = 0;
@@ -70,8 +76,13 @@ struct metadata* find_first_free_node(const uint64_t size) {
             node = first_node;
             continue;
         }
-        node = node->next_node;
+        if (node->next_node == NULL) {
+            struct metadata *next = (void*)node + node->size;
+            next->is_free = 1;
+            next->size = page_size - (page + sizeof(struct metadata));
+        }
         page -= sizeof(struct metadata) + node->size;
+        node = node->next_node;
     }
 
     return node;
@@ -87,7 +98,7 @@ void* my_malloc(const size_t size) {
     struct metadata *mdata = find_first_free_node(size);
     __asan_unpoison_memory_region(mdata + 1, mdata->size);
 
-    struct metadata *next_node = (void*)mdata + size + sizeof(struct metadata);
+    struct metadata *next_node = (void*)(mdata + 1) + size;
     next_node->size = mdata->size - size - sizeof(struct metadata);
     next_node->is_free = 1;
     mdata->is_free = 0;
@@ -101,5 +112,5 @@ void* my_malloc(const size_t size) {
 void my_free(void *ptr) {
     struct metadata *mdata = (struct metadata*)ptr - 1;
     mdata->is_free = 1;
-    __asan_poison_memory_region(mdata, mdata->size + sizeof(struct metadata));
+    __asan_poison_memory_region(mdata + 1, mdata->size);
 }
